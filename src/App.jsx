af1b754c837cba8525e00fb8bdcdf9f4d0a85763
Gerard <gerard.eklu@yahoo.fr>
2025-04-03 14:24:19 -0400

    Setting up the firebase

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,12 @@
 import React from 'react';
-import {Route, Routes} from 'react-router-dom';
+import { Route, Routes } from 'react-router-dom';
 import Home from './routes/home/home.component';
 import Navigation from './routes/navigation/navigation.component';
+import SignIn from './routes/sign-in/sign-in.component';
 
 const Shop = () => {
-  return (
-    <div>Shopping Page component</div>
-  )
-}
+  return <div>Shopping Page component</div>;
+};
 
 const App = () => {
   return (
@@ -16,10 +15,11 @@ const App = () => {
         <Route path='/' element={<Navigation />}>
           <Route index element={<Home />} />
           <Route path='/shop' element={<Shop />} />
+          <Route path='/sign-in' element={<SignIn />} />
         </Route>
       </Routes>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
--- src/routes/navigation/navigation.component.jsx
+++ src/routes/navigation/navigation.component.jsx
@@ -3,25 +3,25 @@ import { Outlet, Link } from 'react-router-dom';
 import logo from '../../assets/logo.png';
 import './navigation.styles.scss';
 
-
 const Navigation = () => {
-   return (
-     <Fragment>
-        <div className='navigation'>
-         <Link className="logo-container" to="/">
-           <img src={logo} className='logo' />
-         </Link>
-         <div className='nav-links-container'>
-            <Link className="nav-link" to="/shop">
-              SHOP
-            </Link>
-         </div>
-       </div>
-       <Outlet />
-     </Fragment>
-    
-   )
- }
+  return (
+    <Fragment>
+      <div className='navigation'>
+        <Link className='logo-container' to='/'>
+          <img src={logo} className='logo' />
+        </Link>
+        <div className='nav-links-container'>
+          <Link className='nav-link' to='/shop'>
+            SHOP
+          </Link>
+          <Link className='nav-link' to='/sign-in'>
+            SIGN IN
+          </Link>
+        </div>
+      </div>
+      <Outlet />
+    </Fragment>
+  );
+};
 
- 
 export default Navigation;
